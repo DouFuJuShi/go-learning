4f05655f9ea9549e328bf1fdef93c670ec446180
豆腐居士 <452158342@qq.com>
2023-11-08 12:01:08 +0800

    Create main.go

---
+++ RocketMQ/main.go
@@ -0,0 +1,93 @@
+/*
+https://github.com/apache/rocketmq-client-go/blob/master/examples/producer/transaction/main.go
+*/
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strconv"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/apache/rocketmq-client-go/v2"
+	"github.com/apache/rocketmq-client-go/v2/primitive"
+	"github.com/apache/rocketmq-client-go/v2/producer"
+)
+
+type DemoListener struct {
+	localTrans       *sync.Map
+	transactionIndex int32
+}
+
+func NewDemoListener() *DemoListener {
+	return &DemoListener{
+		localTrans: new(sync.Map),
+	}
+}
+
+func (dl *DemoListener) ExecuteLocalTransaction(msg *primitive.Message) primitive.LocalTransactionState {
+	nextIndex := atomic.AddInt32(&dl.transactionIndex, 1)
+	fmt.Printf("nextIndex: %v for transactionID: %v\n", nextIndex, msg.TransactionId)
+	status := nextIndex % 3
+	dl.localTrans.Store(msg.TransactionId, primitive.LocalTransactionState(status+1))
+
+	fmt.Printf("dl")
+	return primitive.UnkonwnState
+}
+
+func (dl *DemoListener) CheckLocalTransaction(msg *primitive.MessageExt) primitive.LocalTransactionState {
+	fmt.Printf("%v msg transactionID : %v\n", time.Now(), msg.TransactionId)
+	v, existed := dl.localTrans.Load(msg.TransactionId)
+	if !existed {
+		fmt.Printf("unknown msg: %v, return Commit", msg)
+		return primitive.CommitMessageState
+	}
+	state := v.(primitive.LocalTransactionState)
+	switch state {
+	case 1:
+		fmt.Printf("checkLocalTransaction COMMIT_MESSAGE: %v\n", msg)
+		return primitive.CommitMessageState
+	case 2:
+		fmt.Printf("checkLocalTransaction ROLLBACK_MESSAGE: %v\n", msg)
+		return primitive.RollbackMessageState
+	case 3:
+		fmt.Printf("checkLocalTransaction unknown: %v\n", msg)
+		return primitive.UnkonwnState
+	default:
+		fmt.Printf("checkLocalTransaction default COMMIT_MESSAGE: %v\n", msg)
+		return primitive.CommitMessageState
+	}
+}
+
+func main() {
+	p, _ := rocketmq.NewTransactionProducer(
+		NewDemoListener(),
+		producer.WithNsResolver(primitive.NewPassthroughResolver([]string{"127.0.0.1:9876"})),
+		producer.WithRetry(1),
+	)
+	err := p.Start()
+	if err != nil {
+		fmt.Printf("start producer error: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	for i := 0; i < 10; i++ {
+		res, err := p.SendMessageInTransaction(context.Background(),
+			primitive.NewMessage("TopicTest5", []byte("Hello RocketMQ again "+strconv.Itoa(i))))
+
+		if err != nil {
+			fmt.Printf("send message error: %s\n", err)
+		} else {
+			fmt.Printf("send message success: result=%s\n", res.String())
+		}
+	}
+	time.Sleep(5 * time.Minute)
+	err = p.Shutdown()
+	if err != nil {
+		fmt.Printf("shutdown producer error: %s", err.Error())
+	}
+}
